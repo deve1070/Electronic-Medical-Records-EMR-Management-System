2f0d36e236ddf5d96b0432845d14b2cbe8e144e0
deve1070 <dkassa253@gmail.com>
2025-05-08 14:22:18 +0300

    some error

--- emr-system/lib/prisma.ts
+++ emr-system/lib/prisma.ts
@@ -1,14 +1,18 @@
 import { PrismaClient } from '@prisma/client';
 
-// Extend globalThis to include prisma
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const prisma = global.prisma || new PrismaClient();
+let prisma: PrismaClient;
 
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma;
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!globalForPrisma.prisma) {
+    globalForPrisma.prisma = new PrismaClient();
+  }
+  prisma = globalForPrisma.prisma;
 }
 
 export default prisma;
